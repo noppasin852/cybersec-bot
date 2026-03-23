e8a3f181ae1566b59025dafd8898b03874250e24
noppasin852 <60421943+noppasin852@users.noreply.github.com>
2026-03-23 18:50:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 // รับ JSON
 app.use(express.json());
+
 app.use("/auth", require("./routes/auth"));
-// เชื่อม MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB connected"))
   .catch(err => console.log("DB error:", err));
