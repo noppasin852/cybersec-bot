16c71fec4f0f01218c5a68a4b86b036b0eafae83
noppasin852 <60421943+noppasin852@users.noreply.github.com>
2026-03-23 18:38:52 +0700

    create server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// รับ JSON
+app.use(express.json());
+
+// เชื่อม MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log("DB error:", err));
+
+// ใช้ auth route
+app.use("/auth", require("./routes/auth"));
+
+// หน้า test
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+// เปิด server
+app.listen(3000, () => {
+  console.log("Server running");
+});
