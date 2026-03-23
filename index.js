f9f8421bc3bd3db5f416d6ae5977ee66fd42735b
noppasin852 <60421943+noppasin852@users.noreply.github.com>
2026-03-23 19:09:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-client.on("ready", () => {
+client.on("clientReady", () => {
   console.log("Bot online");
 });
 
